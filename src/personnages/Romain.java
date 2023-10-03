57a0d96d18499c0a37fea6af09cadea154515020
Marwa Dadjo <ddm4766a@ensfsi.onmicrosoft.com>
2023-10-03 17:54:43 +0200

    fin TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,15 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+	Equipement[] equipement ; 
+	private int nbEquipement=0;
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-	}
+		assert force >=0 ; 
+		Equipement[] equipement = new Equipement[nbEquipement];
+		}
+		
 	public String getNom() {
 		return nom;
 	}
@@ -18,15 +22,31 @@ public class Romain {
 		return "Le romain " + nom + " : ";
 	}
 	public void recevoirCoup(int forceCoup) {
+		assert force >= 0 ; 
+		int forceAv = force ;
 		force -= forceCoup ; 
+		assert force < forceAv ;
 		if (force >0) {
 			parler ("Aie");
 		} else {
 			parler ("J'abandonne...");
 		}	
+		
+	
+	}
+	public void sEquiper (Equipement nomEquipement) {
+		// TODO : Faire le switch
+		switch (key) {
+		case value: {
+			
+			yield type;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + key);
+		}
 	}
 	public static void main (String[] args) {
-		Romain unRomain = new Romain ("unRomain", 3);
+		Romain unRomain = new Romain ("unRomain", (6));
 		unRomain.prendreParole();
 		unRomain.parler("Salut");
 		unRomain.recevoirCoup(8);
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,19 +2,58 @@ package personnages;
 
 public class Village {
 	private String nom;
-	private Chef chef ; 
+	private Chef chef ;
+	private Gaulois [] villageois ;
+	private int nbVillageois = 0 ;
+	private int nbVillageoisMax ;
 	
-	public Village (String nom) {
+	public Village (String nom, int nbVillageoisMax) {
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMax];
 	}
 
 	public void setChef(Chef chef) {
 		this.chef = chef;
+	}
+	
+	public void ajoutHabitant (Gaulois nomVillageois) {
+		villageois [nbVillageois] = nomVillageois ;
+		nbVillageois ++ ; 
+	}
+	
+	public String trouverHabitant (int numVillageois) {
+		String nomGaulois = villageois[numVillageois].getNom();
+		return nomGaulois ;
+	}
+	
+	public void afficherVillageois () {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les legendaires gaulois :");
+		int i = 0 ; 
+		while (i<nbVillageois) {
+			System.out.println("- "+ trouverHabitant(i));
+			i++ ; 
+		}
+		
 	}
 
 	public String getNom() {
 		return nom;
 	}
 	
-
+	public static void main (String [] args) {
+		Village village = new Village ("Village des Irreductibles", 30) ; 
+		Chef chef = new Chef("Abraracourcix", 6, village);
+		village.setChef(chef);
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		village.ajoutHabitant(asterix);
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		village.ajoutHabitant(obelix);
+		village.afficherVillageois();
+		/*Gaulois gaulois = village.trouverHabitant(1);
+		System.out.println(gaulois);
+		Erreur car Gaulois doit devenir String et il n'y a pas d'habitant en 1*/
+		/*Gaulois gaulois = village.trouverHabitant(30); 
+		 Erreur car le compilateur prend Gaulois en tant que type et il cherche le type String*/
+		
+	}
 }
